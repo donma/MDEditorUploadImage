a4dbe2a34c00f39742d1f09eed0a7412d7ad9c00
許當麻 <no2don@outlook.com>
2018-07-02 17:29:34 +0800

    add files.

---
+++ MDEditorUploadImage/uploadimage.ashx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace MDEditorUploadImage
+{
+    
+    public class uploadimage : IHttpHandler
+    {
+
+        public class ResultInfo
+        {
+            public int success { get; set; }
+            public string message { get; set; }
+            public string url { get; set; }
+        }
+
+
+        public void ProcessRequest(HttpContext context)
+        {
+            try
+            {
+
+                //儲存路徑
+                DirectoryInfo FileDir = new DirectoryInfo(System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "source\\");
+
+                //如果檔案夾沒有建立則自行建立
+                if (FileDir.Exists == false)
+                {
+                    FileDir.Create();
+                }
+
+                HttpFileCollection uploadedFiles = context.Request.Files;
+
+                if (uploadedFiles.Count > 0)
+                {
+                    HttpPostedFile userPostedFile = uploadedFiles[0];
+
+                    if (userPostedFile.ContentLength > 0)
+                    {
+                        userPostedFile.SaveAs(System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "source" + Path.DirectorySeparatorChar + Path.GetFileName(userPostedFile.FileName));
+                        context.Response.Write(new JavaScriptSerializer().Serialize(new ResultInfo { message = "上傳成功", success = 1, url = "http://"+context.Request.Url.Authority + "/source/" + Path.GetFileName(userPostedFile.FileName) }));
+                    }
+                }
+
+               
+            }
+            catch (Exception ex)
+            {
+                context.Response.Write(new JavaScriptSerializer().Serialize(new ResultInfo { message = "上傳失敗:"+ex.Message, success = 0, url ="" }));
+            }
+        }
+
+        //將收到的stream 寫入檔案
+        private void SaveFile(Stream stream, FileStream fs)
+        {
+            try
+            {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    fs.Write(buffer, 0, bytesRead);
+                }
+            }
+            catch (Exception ex)
+            {
+                //這邊可以放入錯誤的Log
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MDEditorUploadImage/uploadpix.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" %>
+
+<script runat="server" language="C#">
+    public class ResultInfo
+    {
+        public int success { get; set; }
+        public string message { get; set; }
+        public string url { get; set; }
+    }
+</script>
+
+<script runat="server" language="C#">
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+
+            //儲存路徑
+            System.IO.DirectoryInfo FileDir = new System.IO.DirectoryInfo(System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "source\\");
+
+            //如果檔案夾沒有建立則自行建立
+            if (FileDir.Exists == false)
+            {
+                FileDir.Create();
+            }
+
+            HttpFileCollection uploadedFiles = Request.Files;
+
+            if (uploadedFiles.Count > 0)
+            {
+                HttpPostedFile userPostedFile = uploadedFiles[0];
+
+                if (userPostedFile.ContentLength > 0)
+                {
+                    userPostedFile.SaveAs(System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "source" + System.IO.Path.DirectorySeparatorChar + System.IO.Path.GetFileName(userPostedFile.FileName));
+                      Response.Write(new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(new ResultInfo { message = "上傳成功", success = 1, url ="http://"+ Request.Url.Authority + "/source/" + System.IO.Path.GetFileName(userPostedFile.FileName) }));
+                }
+            }
+
+
+        }
+        catch (Exception ex)
+        {
+              Response.Write(new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(new ResultInfo { message = "上傳失敗:" + ex.Message, success = 0, url = "" }));
+        }
+    }
+</script>
+
+
